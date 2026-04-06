ecf241be03b7aec295ae788a68e442ff769dc521
kaiser-114 <areezilla@gmail.com>
2026-04-06 12:34:17 +0700

    new file struct file 4

---
+++ struct4.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+struct alamatdetail
+{
+    string desa;
+    string kota;
+};
+struct mahasiswa
+{
+    string nama;
+    alamatdetail alamat;
+    int umur;
+};
+
+int main()
+{
+    // deklarasi variabel struct
+    mahasiswa mhs[3];
+    //mengisi data
+    for (int i = 0 ; i <= 2; i++)
+    {
+    cout << "isikan data nama : ";
+    getline (cin, mhs[i].nama );
+    cout << "Isikan data desa : ";
+    getline (cin, mhs[i].alamat.desa );
+    cout << "Isikan data kota : ";
+    getline (cin, mhs[i].alamat.kota );
+    cout << "Isikan data usia : ";
+    cin >> mhs[i].umur;
+    cin.ignore();
+    }
+
+    cout << "Data Mahasiswa" << endl;
+    for(int i = 0; i < 2; i++)
+    {
+        cout << "Nama : " << mhs[i].nama << endl;
+        cout << "Kota : " << mhs[i].alamat.kota << endl;
+        cout << "Desa : " << mhs[i].alamat.desa << endl;
+        cout << "Usia : " << mhs[i].umur << endl;
+    }
+}
