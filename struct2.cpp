1b6579fd6c41de8a79859ea7e3cff8dc2bd71520
kaiser-114 <areezilla@gmail.com>
2026-04-06 12:30:29 +0700

    menambahkan int main

--- struct2.cpp
+++ struct2.cpp
@@ -7,3 +7,21 @@ struct mahasiswa
     string alamat;
     int umur;
 };
+
+int main()
+{
+    // deklarasi variabel struct
+    mahasiswa mhs;
+    //mengisi data
+    cout << "isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "Isikan data usia : ";
+    cin >> mhs.umur;
+
+    cout << "data mahasiswa "  << endl;
+    cout << "nama : "  <<mhs.nama << endl;
+    cout << "alamat : "  <<mhs.alamat << endl;
+    cout << "umur : "  <<mhs.umur << endl;
+}
